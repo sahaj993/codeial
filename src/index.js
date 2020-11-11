5e981397ae1fe18efa1ee06425d733339f667c5c
sahaj993 <sahajseth1998@gmail.com>
2020-11-11 23:58:56 +0530

    Default store added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {configureStore} from './store/index';
+
+const store = configureStore(); 
+console.log('Store',store);
 
 ReactDOM.render(
   <React.StrictMode>
