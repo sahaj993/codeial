d5659f9323d5bd448f4b5ae286eea791258a62be
sahaj993 <sahajseth1998@gmail.com>
2020-11-14 10:12:04 +0530

    PropTypes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './PostsList';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
